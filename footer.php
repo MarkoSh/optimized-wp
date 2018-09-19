754de77d2defbe5b2700daac72e29692128c239d
Mark <dorianmj@gmail.com>
2018-09-19 22:45:14 +0300

    Проверяем вуу

--- footer.php
+++ footer.php
@@ -2,7 +2,11 @@
 			<footer id="colophon" role="contentinfo"></footer><!-- #colophon -->
 		</div><!-- #page -->
 
-	<?php woocommerce_photoswipe(); ?>
+	<?php
+		if ( function_exists( 'woocommerce_photoswipe' ) ) {
+			woocommerce_photoswipe();
+		}
+	?>
 
 	<script src="<?php echo get_template_directory_uri(); ?>/scripts.min.js"></script>
 
