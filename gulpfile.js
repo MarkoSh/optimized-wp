b307eeb367471d1b50a54bb888139e6f9d946a7f
Mark <dorianmj@gmail.com>
2018-10-22 21:42:52 +0300

    - Проверяем размер выходных файлов и перезапускаем таски если все плохо - Карта для js не очень нужна

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp          = require( "gulp" ),
+let fs			  = require( "fs" ),
+	gulp          = require( "gulp" ),
 	gutil         = require( "gulp-util" ),
 	scss          = require( "gulp-sass" ),
 	concat        = require( "gulp-concat" ),
@@ -12,37 +13,53 @@ var gulp          = require( "gulp" ),
 
 gulp.task( "scss", () => {
 	return gulp.src( "style.scss" )
-	.pipe( scss( { outputStyle: "expand" } ).on( "error", notify.onError() ) )
-	.pipe( rename( { suffix: ".min", prefix : "" }))
-	.pipe( autoprefixer( [ "last 15 versions" ] ).on( "error", notify.onError() ) )
-	.pipe( cleancss( { level: { 1: { specialComments: 0 } } } ).on( "error", notify.onError() ) ) // Opt., comment out when debugging
-	.pipe( gulp.dest( "." ) );
+		.pipe( scss( { outputStyle: "expand" } ).on( "error", notify.onError() ) )
+		.pipe( rename( { suffix: ".min", prefix : "" }))
+		.pipe( autoprefixer( [ "last 15 versions" ] ).on( "error", notify.onError() ) )
+		.pipe( cleancss( { level: { 1: { specialComments: 0 } } } ).on( "error", notify.onError() ) ) // Opt., comment out when debugging
+		.pipe( gulp.dest( "." ) );
 } )
 
 gulp.task( "js", () => {
 	return gulp.src( [
-		"node_modules/axios/dist/axios.min.js",
+			"node_modules/axios/dist/axios.min.js",
 
-		"node_modules/inputmask/dist/min/inputmask/dependencyLibs/inputmask.dependencyLib.min.js",
-		"node_modules/inputmask/dist/min/inputmask/inputmask.min.js",
-		"node_modules/inputmask/dist/min/inputmask/inputmask.extensions.min.js",
-		"node_modules/inputmask/dist/min/inputmask/inputmask.phone.extensions.min.js",
+			"node_modules/inputmask/dist/min/inputmask/dependencyLibs/inputmask.dependencyLib.min.js",
+			"node_modules/inputmask/dist/min/inputmask/inputmask.min.js",
+			"node_modules/inputmask/dist/min/inputmask/inputmask.extensions.min.js",
+			"node_modules/inputmask/dist/min/inputmask/inputmask.phone.extensions.min.js",
 
-		"node_modules/photoswipe/dist/photoswipe.min.js",
-		"node_modules/photoswipe/dist/photoswipe-ui-default.min.js",
+			"node_modules/photoswipe/dist/photoswipe.min.js",
+			"node_modules/photoswipe/dist/photoswipe-ui-default.min.js",
 
-		"node_modules/swiper/dist/js/swiper.min.js",
+			"node_modules/swiper/dist/js/swiper.min.js",
 
-		"js/scripts.js", // Always at the end
+			"js/scripts.js", // Always at the end
 		] )
-	.pipe( concat( "scripts.min.js" ) )
-	.pipe( uglify().on( "error", notify.onError() ) ) // Mifify js (opt.) - mifify hahaha
-	.pipe( gulp.dest( "." ) );
+		.pipe( concat( "scripts.min.js" ) )
+		.pipe( uglify().on( "error", notify.onError() ) ) // Mifify js (opt.) - mifify hahaha
+		.pipe( gulp.dest( "." ) );
 } )
 
 gulp.task( "watch", [ "scss", "js" ], () => {
 	gulp.watch( "**/*.scss", [ "scss" ] );
 	gulp.watch( "js/scripts.js", [ "js" ] );
+
+	fs.watchFile( "style.min.css", {
+		interval: 100
+	}, ( current, previous ) => {
+		if ( current.size == 0 ) {
+			gulp.start( 'scss' );
+		}
+	} );
+
+	fs.watchFile( "scripts.min.js", {
+		interval: 100
+	}, ( current, previous ) => {
+		if ( current.size == 0 ) {
+			gulp.start( 'js' );
+		}
+	} );
 } );
 
 gulp.task( "default", [ "watch" ] );
